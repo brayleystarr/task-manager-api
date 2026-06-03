a61fd0a4e9b60a7de6f3b3edae15769b4c3101d2
Brayley <brayleydstarr@gmail.com>
2026-06-03 01:09:18 +0000

    Add task model with SQLAlchemy ORM

---
+++ app/db/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# declare connective string
+DATABASE_URL = "postgresql+psycopg2://taskuser:password@localhost:5432/taskdb"
+
+# init engine 
+engine = create_engine(DATABASE_URL)
+
+# init session object factory
+SessionLocal = sessionmaker(
+    bind=engine, 
+    autoflush=False, 
+    autocommit=False
+)
+
+# init registry for all model metadata for table creation 
+Base = declarative_base()
+
+
+
+
+
---
+++ app/models/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime 
+from app.db.database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+
+class Task(Base):
+    __tablename__ = "tasks"
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
